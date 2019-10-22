4decbcc8e24002084cea5e034c843973aec8f91f
edufgar <35237258+edufgar@users.noreply.github.com>
2019-10-22 15:58:03 -0300

    Add files via upload

---
+++ Trabalho Universia/Trabalho 4 Bimestre Universia.sql
@@ -0,0 +1,82 @@
+CREATE DATABASE Universia
+
+USE Universia
+
+CREATE TABLE Alunos
+(
+matricula INT IDENTITY,
+nomealuno VARCHAR(20)
+)
+ 
+INSERT Alunos (nome) VALUES ('')
+GO 
+CREATE TABLE Cursos
+(
+codcurso INT IDENTITY,
+nomecurso VARCHAR(50)
+)
+INSERT Cursos (nomecurso) VALUES ('')--1
+SELECT @@IDENTITY
+ 
+CREATE TABLE GRADE
+(
+codigocurso INT,
+codigograde INT,
+codigodisc INT,
+anocurso int
+)
+
+INSERT GRADE (codigocurso, codigograde,
+codigodisc,ano)
+VALUES 
+(1,1,1,2019),
+(1,1,2,2019)
+ 
+CREATE TABLE DISCIPLINA
+(
+codigodisc INT IDENTITY,
+nomedisc VARCHAR(20)
+)
+
+INSERT DISCIPLINA (nomedisc) VALUES ('')
+DECLARE @cod_disc INT 
+SET @cod_disc = (SELECT @@IDENTITY)
+SELECT @cod_disc
+
+
+INSERT DISCIPLINA (nomedisc) VALUES ('')
+SELECT @@IDENTITY
+
+SELECT * FROM Alunos
+SELECT * FROM Cursos--1
+SELECT * FROM GRADE
+
+CREATE TABLE MATRICULA 
+(
+matricula INT,
+codcurso INT,
+anocurso INT,
+codigodisc int
+)
+
+CREATE PROCEDURE spMatriculaAluno
+@nomealuno varchar(20),
+@curso varchar(20),
+@anocurso int
+AS
+BEGIN
+	DECLARE @matricula INT 
+
+	SET @matricula = 
+	(SELECT matricula FROM Alunos WHERE nomealuno = @nomealuno)
+
+	INSERT MATRICULA (matricula, codcurso, anocurso, codigodisc)
+	SELECT @matricula, codigocurso, @anocurso,codigodisc
+	FROM GRADE LEFT JOIN Cursos 
+	ON GRADE.codigocurso = Cursos.codcurso
+END
+
+EXEC spMatriculaAluno '','',2019
+
+SELECT * FROM Alunos
+
---
+++ Trabalho Universia/Trigger e Funções.sql
@@ -0,0 +1,31 @@
+USE MinhaCaixa
+
+--- Trigger
+
+CREATE TRIGGER trgINSERT_CLIENTE
+ON Clientes
+FOR INSERT
+AS
+BEGIN
+INSERT clientes_audit 
+(ClienteCodigo,ClienteCPF,ClienteNome)
+SELECT ClienteCodigo,ClienteCPF, ClienteNome 
+FROM  Inserted
+END
+
+--- Funções
+
+CREATE FUNCTION fnRetornaAno (@data DATETIME)
+RETURNS int
+AS
+  BEGIN
+  DECLARE @ano int
+  SET @ano = YEAR(@data)
+
+      RETURN @ano
+
+END
+
+
+
+
